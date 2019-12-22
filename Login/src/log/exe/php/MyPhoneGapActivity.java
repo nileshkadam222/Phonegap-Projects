1f9bed78bc10bc0724fad018b3afd28ca7a0b8f6
Nilesh <nilesh.kadam222@gmail.com>
2019-12-22 11:37:15 +0530

    Phonegap projects

---
+++ Login/src/log/exe/php/MyPhoneGapActivity.java
@@ -0,0 +1,12 @@
+package log.exe.php;
+
+import org.apache.cordova.DroidGap;
+import android.os.Bundle;
+
+public class MyPhoneGapActivity extends DroidGap {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		super.loadUrl("file:///android_asset/www/index.html");
+	}
+}
---
+++ myproject/gen/nil/a/b/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package nil.a.b;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
